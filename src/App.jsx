65916d84a3c953190308983776766be28e42dcd2
Thomas Clark <tomreeveclark@gmail.com>
2024-11-21 14:58:53 +1100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,22 @@ import { useState, useEffect, useRef } from "react";
 [] record video
 [] take photos
 [] record GPS position
+
+
+navigator.mediaDevices
+        .getUserMedia({
+          video: {
+            facingMode: "environment", // This sets the front-facing camera
+          },
+        })
+        .then(function (stream) {
+          document.getElementById("video").srcObject = stream;
+        })
+        .catch(function (err) {
+          console.error("Camera access denied:", err);
+        });
+
+
 */
 
 function App() {
@@ -17,7 +33,11 @@ function App() {
     // Request access to the camera
     const getCameraStream = async () => {
       try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: {
+            facingMode: "environment",
+          },
+        });
         if (videoRef.current) {
           videoRef.current.srcObject = stream;
         }
@@ -56,7 +76,7 @@ function App() {
       <div style={{ width: "100%", display: "flex", backgroundColor: "blue", flex: 1 }}>
         <video ref={videoRef} autoPlay muted />
 
-        <iframe src="https://connect.ap.app.pam.co/?navmapId=9be4fec5-1945-11ee-9083-0a17c8c3f95c" allow="geolocation; accelerometer;  gyroscope; magnetometer"></iframe>
+        {/* <iframe src="https://connect.ap.app.pam.co/?navmapId=9be4fec5-1945-11ee-9083-0a17c8c3f95c" allow="geolocation; accelerometer;  gyroscope; magnetometer"></iframe> */}
       </div>
     </>
   );
